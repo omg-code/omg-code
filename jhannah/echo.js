347b1d8ecec9b36a4f77a96590b5f453a9526cf8
Jay Hannah <jay.hannah@iinteractive.com>
2013-06-24 19:51:05 -0500

    Echo demo

---
+++ jhannah/echo.js
@@ -0,0 +1,9 @@
+var net = require('net');
+
+var server = net.createServer(function (socket) {
+  socket.write('Echo server\r\n');
+  socket.pipe(socket);
+});
+
+server.listen(1338, '10.0.0.198');
+
