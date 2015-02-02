218f95100fa7456842e602cb9dd630b53d8ce62b
David Knaack <davidknaack@gmail.com>
2015-02-02 09:04:11 -0600

    mostly solves 1-7, time-based challenges

--- elevatorsaga/dknaack.js
+++ elevatorsaga/dknaack.js
@@ -1,39 +1,53 @@
-
 {
-    init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+    init: function(elevators, floors) {         
         
-        elevator.on("idle", function() {
-        });
-
-        elevator.doQ = function(){
-             if(elevator.getPressedFloors().length > 0) {
-                 elevator.getPressedFloors().forEach(function(n){
-                     if(elevator.destinationQueue.indexOf(n)==-1){
-                         console.log("queue is : " + elevator.destinationQueue );
-                         console.log("adding "+n);
-                        elevator.goToFloor(n)
-                     }
-                 });
-                 console.log("queue: " + elevator.destinationQueue);
-             }
+        var dispatchCall = function(floor){
+            var needStop = true;
+                
+            // Is the requested floor already an eleveator destination?
+            elevators.forEach(function(ev){
+                if((ev.destinationQueue.indexOf(floor)>-1)&&(ev.loadFactor<1)){
+                    needStop = false;
+                }
+            });
+               
+            if(needStop){
+                var pickedE = 0;
+                elevators.forEach(function(ele){
+                    if(ele.destinationQueue.length < elevators[pickedE].destinationQueue.length)
+                        pickedE = ele.num;
+                });
+                
+                elevators[pickedE].goToFloor(floor);
+            }
+            
+            elevators.forEach(function(ele){
+                ele.doQ();
+            });
         };
+       
+        elevators.forEach(function(e,num){
+            var elevator = e; 
+            elevator.num = num;
+
+            elevator.goingUpIndicator(true);
+            elevator.goingDownIndicator(true);
+        
+            elevator.doQ = function(){
+                elevator.getPressedFloors().forEach(function(n) {
+                    if((elevator.loadFactor() < 1) && (elevator.destinationQueue.indexOf(n)==-1)){
+                        elevator.goToFloor(n)
+                        elevator.destinationQueue.sort()
+                        elevator.checkDestinationQueue()
+                    }
+                });
+            };
+        });
         
         floors.forEach(function(f,n){
-           f.on("up_button_pressed", function() {
-               elevator.goToFloor(n);
-               console.log("call: up from " + n);
-               elevator.doQ();
-            });
-           f.on("down_button_pressed", function(e) {
-               elevator.goToFloor(n);
-               console.log("call: dn from " + n);
-               elevator.doQ();
-           });
+            f.on("up_button_pressed", function() { dispatchCall(n);  });
+            f.on("down_button_pressed", function() { dispatchCall(n); });
         });
-        
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+    update: function(dt, elevators, floors) {}
 }
